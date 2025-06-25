bf175bce497420c5b543e93f0d6e85b69f2a703a
Raphael Eduardo <raphaeleduardosc@gmail.com>
2025-06-25 03:58:18 -0300

    DTO

---
+++ backend/src/main/java/com/devraphael/dscatalog/dto/CategoryDTO.java
@@ -0,0 +1,31 @@
+package com.devraphael.dscatalog.dto;
+
+import com.devraphael.dscatalog.entities.Category;
+
+public class CategoryDTO {
+
+	private Long id;
+	private String name;
+
+	public CategoryDTO() {
+	}
+
+	public CategoryDTO(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
+	public CategoryDTO(Category entity) {
+		id = entity.getId();
+		name = entity.getName();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+}
