64941c8d33beb768e4cfb1e2d0840c32c9083967
Raphael Eduardo <raphaeleduardosc@gmail.com>
2025-07-08 20:10:24 -0300

    Starting recover token

---
+++ backend/src/main/java/com/devraphael/dscatalog/services/AuthService.java
@@ -0,0 +1,13 @@
+package com.devraphael.dscatalog.services;
+
+import org.springframework.stereotype.Service;
+
+import com.devraphael.dscatalog.dto.EmailDTO;
+
+@Service
+public class AuthService {
+
+	public void createRecoverToken(EmailDTO body) {		
+	}
+
+}
