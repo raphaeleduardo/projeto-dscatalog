601edfc8dbd859fe4ffff150655cf4c541274e6c
Raphael Eduardo <raphaeleduardosc@gmail.com>
2025-07-08 23:24:06 -0300

    Save new password with valid token

--- backend/src/main/java/com/devraphael/dscatalog/controllers/AuthController.java
+++ backend/src/main/java/com/devraphael/dscatalog/controllers/AuthController.java
@@ -3,11 +3,13 @@ package com.devraphael.dscatalog.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devraphael.dscatalog.dto.EmailDTO;
+import com.devraphael.dscatalog.dto.NewPasswordDTO;
 import com.devraphael.dscatalog.services.AuthService;
 
 import jakarta.validation.Valid;
@@ -18,10 +20,16 @@ public class AuthController {
 
 	@Autowired
 	private AuthService authService;
-	
+
 	@PostMapping(value = "/recover-token")
 	public ResponseEntity<Void> createRecoverToken(@Valid @RequestBody EmailDTO body) {
 		authService.createRecoverToken(body);
+		return ResponseEntity.noContent().build();
+	}
+
+	@PutMapping(value = "/new-password")
+	public ResponseEntity<Void> saveNewPassword(@Valid @RequestBody NewPasswordDTO body) {
+		authService.saveNewPassword(body);
 		return ResponseEntity.noContent().build();
 	}
 
--- backend/src/main/java/com/devraphael/dscatalog/services/AuthService.java
+++ backend/src/main/java/com/devraphael/dscatalog/services/AuthService.java
@@ -1,14 +1,17 @@
 package com.devraphael.dscatalog.services;
 
 import java.time.Instant;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devraphael.dscatalog.dto.EmailDTO;
+import com.devraphael.dscatalog.dto.NewPasswordDTO;
 import com.devraphael.dscatalog.entities.PasswordRecover;
 import com.devraphael.dscatalog.entities.User;
 import com.devraphael.dscatalog.repositories.PasswordRecoverRepository;
@@ -23,6 +26,9 @@ public class AuthService {
 
 	@Value("${email.password-recover.uri}")
 	private String recoverUri;
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	@Autowired
 	private UserRepository userRepository;
@@ -48,10 +54,22 @@ public class AuthService {
 		entity.setExpiration(Instant.now().plusSeconds(tokenMinutes * 60));
 		entity = passwordRecoverRepository.save(entity);
 
-		String text = "Acesse o link para definir uma nova senha \n\n" + recoverUri + token 
-				+ "\n\n Validade de " + tokenMinutes + " minutos";
+		String text = "Acesse o link para definir uma nova senha \n\n" + recoverUri + token + "\n\n Validade de "
+				+ tokenMinutes + " minutos";
 
-				emailService.sendEmail(body.getEmail(), "Recuperação de senha", text);
+		emailService.sendEmail(body.getEmail(), "Recuperação de senha", text);
+	}
+
+	@Transactional
+	public void saveNewPassword(NewPasswordDTO body) {
+		List<PasswordRecover> result = passwordRecoverRepository.searchValidTokens(body.getToken(), Instant.now());
+		if (result.size() == 0) {
+			throw new ResourceNotFoundException("Token inválido");
+		}
+
+		User user = userRepository.findByEmail(result.get(0).getEmail());
+		user.setPassword(passwordEncoder.encode(body.getPassword()));
+		user = userRepository.save(user);
 	}
 
 }
